6056a197e7dc110633b6ff391a1f02488b398639
BoonChinLook <boon.look3@mail.dcu.ie>
2023-05-01 15:01:35 +0100

    First GitHub Push

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def about():
+    return render_template('page2.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
