94dce7cc8fb8a18f92709acf15c160fdbc0404e6
BoonChinLook <boon.look3@mail.dcu.ie>
2023-06-05 20:45:23 +0100

    added dark-mode as wanted

--- static/page2.js
+++ static/page2.js
@@ -1,6 +1,7 @@
 var darkModeToggle = document.getElementById("dark-mode-toggle");
 darkModeToggle.addEventListener("click", function() {
   document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-theme");
 });
 
 
